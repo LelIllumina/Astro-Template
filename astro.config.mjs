8b8eff2a13ead10fa4b5622835a379dd6f213c42
Lel Illumina <dragonbilal938@outlook.com>
2025-07-11 01:04:01 +0500

    fix: astro config and readme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://your.site.here",
+  output: "static",
+  //   server: { host: true }, // uncomment if live server dosent work for some reason
+});
